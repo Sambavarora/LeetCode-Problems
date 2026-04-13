808a559fa4bb410d4409002739f2500a2c64b2e9
Sambavarora <sambavarora2005@outlook.com>
2026-04-13 15:17:34 +0530

    Day 8: PTOD MIn Distance

---
+++ Arrays/Minimum Distance to the Target Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                int index=i;
+                ans=min(ans,abs(index-start));
+            }
+        }
+        return ans;
+    }
+};
