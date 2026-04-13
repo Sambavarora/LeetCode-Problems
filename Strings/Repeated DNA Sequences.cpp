e370c6a711cc6d3d44156c4637cc6c34116f4c0d
Sambavarora <sambavarora2005@outlook.com>
2026-04-13 15:37:07 +0530

    Day 8: Q2 used(Hash table) repeated dna seq.

---
+++ Strings/Repeated DNA Sequences.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string,int>mp;
+        vector<string>ans;
+        if(s.size() < 10) return ans;
+        for(int i=0;i<=s.size()-10;i++){
+            mp[s.substr(i,10)]++;
+        }
+        for(auto it:mp){
+            if(it.second>1){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
