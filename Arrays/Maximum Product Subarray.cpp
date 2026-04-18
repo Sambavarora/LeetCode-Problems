f34590f67a1b2bcd30b2a7f04a942f437ed6d8fd
Sambavarora <sambavarora2005@outlook.com>
2026-04-18 18:26:21 +0530

    Day 14: (using Kadane's algo approach)max product

---
+++ Arrays/Maximum Product Subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans=nums[0];
+        int maxproduct=nums[0];
+        int minproduct=nums[0];
+        for(int i=1;i<nums.size();i++){
+            
+            if(nums[i]<0){
+                swap(minproduct,maxproduct);
+                maxproduct=max(nums[i],maxproduct*nums[i]);
+                minproduct=min(nums[i],minproduct*nums[i]);
+            }
+            else{
+                maxproduct=max(nums[i],maxproduct*nums[i]);
+                minproduct=min(nums[i],minproduct*nums[i]);
+            }
+            ans=max(ans,maxproduct);
+        }
+        return ans;
+    }
+};
