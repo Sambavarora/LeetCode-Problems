833be2033fb68c3471b8d7d2b14eb90ec2382635
Sambavarora <sambavarora2005@outlook.com>
2026-04-23 18:04:06 +0530

    Day 19:Q2 Search in array

---
+++ Arrays/Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto it:mp){
+            if(it.first==target){
+                return true;
+            }
+        }
+        return false;
+    }
+};
