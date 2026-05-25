1095bcbde7020ede22fa6b3db1f5dd9eb10edab9
Sambavarora <sambavarora2005@outlook.com>
2026-05-25 23:23:39 +0530

    Day 48:Most frequent number after key in an array

---
+++ Arrays/Most Frequent Number Following Key In an Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mostFrequent(vector<int>& nums, int key) {
+                unordered_map<int,int>mp;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==key){
+            mp[nums[i+1]]++;
+            }
+        }
+        int ans=-1;
+        int maxi=0;
+        for(auto it:mp){
+            if(it.second>maxi){
+                maxi=it.second;
+                ans=it.first;
+            }
+        }
+        return ans;
+    }
+};
