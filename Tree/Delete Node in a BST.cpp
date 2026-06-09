b9255933275394a20be2f1079834377a97820284
Sambavarora <sambavarora2005@outlook.com>
2026-06-09 13:13:05 +0530

    Day61:Q2 Delete Node in a  BST

---
+++ Tree/Delete Node in a BST.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int Successor(TreeNode*root){
+    int result=root->val;
+    TreeNode*temp=root;
+    while(temp!=NULL){
+        result=temp->val;
+        temp=temp->left;
+    }
+    return result;
+}
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL){
+            return root;
+        }
+        else if(key<root->val){
+            root->left=deleteNode(root->left,key);
+        }
+        else if(key>root->val){
+            root->right=deleteNode(root->right,key);
+        }
+        else{
+            if(root->left==NULL){
+                return root->right;
+            }
+            else if(root->right==NULL){
+                return root->left;
+            }
+            else{
+                root->val=Successor(root->right);
+                root->right=deleteNode(root->right,root->val);
+            }
+        }
+        return root;
+    }
+};
