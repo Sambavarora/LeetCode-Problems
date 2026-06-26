83a0eedba258dcca511d3de0b8cb44ca31878468
Sambavarora <sambavarora2005@outlook.com>
2026-06-26 18:20:13 +0530

    Day 77:(bellman-ford)cheapest flight with k stops

---
+++ Graph/Cheapest flight with k stops.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int>cost(n,INT_MAX);
+        cost[src]=0;
+        for(int i=0;i<=k;i++){
+            vector<int>temp=cost;
+            for(int j=0;j<flights.size();j++){
+            int u=flights[j][0];
+            int v=flights[j][1];
+            int w=flights[j][2];
+                if(cost[u]!=INT_MAX&&cost[u]+w<temp[v]){
+                    temp[v]=cost[u]+w;
+                }
+            } 
+            cost=temp;
+        }
+        if(cost[dst]==INT_MAX){
+            return -1;
+        }
+        return cost[dst];
+    }
+};
