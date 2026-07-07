a122ed06e125c60dfdce16849695ba95180aebbc
Sambavarora <sambavarora2005@outlook.com>
2026-07-07 21:00:57 +0530

    Day 84:Unique Paths 2

---
+++ Recursion/Unique Paths II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+int path(int m,int n,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp){
+    if(m<0||n<0)return 0;
+    if(obstacleGrid[m][n]==1)return 0;
+    if(m==0&&n==0)return 1;
+    if(dp[m][n]!=-1)return dp[m][n];
+    int a=path(m-1,n,obstacleGrid, dp);
+    int b=path(m,n-1,obstacleGrid, dp);
+    dp[m][n]=a+b;
+    return dp[m][n];
+}
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return path(m-1,n-1,obstacleGrid,dp);
+    }
+};
